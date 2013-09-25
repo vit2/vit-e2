108ba86e1621496d1bd33241f30f6365d64e3e57
vit2 <you@example.com>
2013-09-25 22:14:25 +0200

    initial unibox add

--- lib/python/Components/RcModel.py
+++ lib/python/Components/RcModel.py
@@ -8,7 +8,12 @@ class RcModel:
 	RCTYPE_VU = 4
 	RCTYPE_ET4X00 = 5
 	RCTYPE_XP1000 = 6
-
+	RCTYPE_INI0 = 7
+	RCTYPE_INI1 = 8
+	RCTYPE_INI2 = 9
+	RCTYPE_INI3 = 10
+	RCTYPE_INI4 = 11
+	
 	def __init__(self):
 		self.currentRcType = self.RCTYPE_DMM
 		self.readRcTypeFromProc()
@@ -45,7 +50,20 @@ class RcModel:
 					self.currentRcType = self.RCTYPE_ET4X00
 				elif rc == '14':
 					self.currentRcType = self.RCTYPE_XP1000
-
+			elif model == 'ini-3000':
+				fp_version = str(getFPVersion())
+				if fp_version.startswith('1'):
+					self.currentRcType = self.RCTYPE_INI0
+				else:
+					self.currentRcType = self.RCTYPE_INI2
+			elif model == 'ini-5000' or model == 'ini-7000' or model == 'ini-7012' or model== 'ini-9000':
+				self.currentRcType = self.RCTYPE_INI1
+			elif model == 'ini-1000' or model == 'ini-1000ru' or model == 'ini-5000ru' or model== 'ini-9000ru':
+				self.currentRcType = self.RCTYPE_INI2
+			elif model == 'ini-1000sv' or model == 'ini-5000sv':
+				self.currentRcType = self.RCTYPE_INI3
+			elif model == 'ini-1000de':
+				self.currentRcType = self.RCTYPE_INI4
 		elif os.path.exists('/proc/stb/info/vumodel'):
 			self.currentRcType = self.RCTYPE_VU
 
@@ -62,5 +80,15 @@ class RcModel:
 			return '/usr/share/enigma2/rc_models/xp1000/'
 		elif self.currentRcType == self.RCTYPE_VU:
 			return '/usr/share/enigma2/rc_models/vu/'
+		elif self.currentRcType == self.RCTYPE_INI0:
+			return '/usr/share/enigma2/rc_models/ini0/'
+		elif self.currentRcType == self.RCTYPE_INI1:
+			return '/usr/share/enigma2/rc_models/ini1/'
+		elif self.currentRcType == self.RCTYPE_INI2:
+			return '/usr/share/enigma2/rc_models/ini2/'
+		elif self.currentRcType == self.RCTYPE_INI3:
+			return '/usr/share/enigma2/rc_models/ini3/'
+		elif self.currentRcType == self.RCTYPE_INI4:
+			return '/usr/share/enigma2/rc_models/ini4/'      
 
 rc_model = RcModel()
